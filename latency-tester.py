f582095fe9592e1ed757dbc86f23aa8085bdb719
Allen, Timothy <tallen@wharton.upenn.edu>
2021-01-12 08:24:26 -0500

    Switch to a pure-Python implementation of ping, which allows this to run smoothly on any OS.

--- latency-tester.py
+++ latency-tester.py
@@ -1,56 +1,42 @@
-import os, sys, pexpect, time, datetime
+import datetime
+import os
+from time import sleep
+
+from ping3 import ping
+
+# SET YOUR PING RESPONSE TIME THRESHOLD HERE, IN SECONDS
+THRESHOLD = 0.25  # 250 milliseconds is the Comcast SLA threshold.
 
 # SET YOUR PING INTERVAL HERE, IN SECONDS
-interval = 1
+INTERVAL = 1
+
+# WHO SHOULD WE RUN THE PING TEST AGAINST
+DESTINATION = "www.google.com"
 
 # LOG TO WRITE TO WHEN PINGS TAKE LONGER THAN THE THRESHOLD SET ABOVE
 i = datetime.datetime.now()
 log_file = "logs/latency-tester." + i.strftime("%Y.%m.%d.%H.%M.%S") + ".log"
 
-# SET YOUR PING RESPONSE TIME THRESHOLD HERE, IN MILLISECONDS
-threshold = 250
-
-# WHO SHOULD WE RUN THE PING TEST AGAINST
-ping_destination = "www.google.com"
-
 
 def write_to_file(file_to_write, message):
     os.makedirs(os.path.dirname(file_to_write), exist_ok=True)
     fh = open(file_to_write, "a")
-    fh.write(message)
+    fh.write(message + "\n")
     fh.close()
 
 
 count = 0
-line = (
-    f"Ping Interval: {interval}, Destination: {ping_destination}, Threshold to Log "
-    f"(msec): {threshold}"
-)
+header = f"Pinging {DESTINATION} every {INTERVAL} secs; threshold: {THRESHOLD} secs."
+print(header)
+write_to_file(log_file, header)
 
-print(line)
-write_to_file(log_file, line)
-
-ping_command = f"ping -i {interval} {ping_destination}"
-
-child = pexpect.spawn(ping_command)
-child.timeout = 1200
-
-while 1:
-    line = child.readline()
-    if not line:
-        break
-
-    if line.startswith(b"ping: unknown host"):
-        print("Unknown host: " + ping_destination)
-        write_to_file(log_file, "Unknown host: " + ping_destination)
-        break
-
-    if count > 0:
-        ping_time = float(line[line.find(b"time=") + 5 : line.find(b" ms")])
-        line = time.strftime("%m/%d/%Y %H:%M:%S") + ": " + str(ping_time)
-        print(str(count) + ": " + line)
-
-        if ping_time > threshold:
-            write_to_file(log_file, line + "\n")
-
+while True:
     count += 1
+    latency = ping(DESTINATION)
+
+    # Do we want to write it to the log?
+    write_log = "Yes" if latency > THRESHOLD else "No"
+    line = f"Pinged {DESTINATION}; latency: {latency} secs; logging: {write_log}"
+    print(line)
+    write_to_file(log_file, line)
+    sleep(INTERVAL)
